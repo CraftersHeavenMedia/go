e236e5ee09d1724622c183f7142009de7d1b9a42
CraftersHeavenMedia <craftersheavenmedia@gmail.com>
2024-09-30 01:09:20 +0530

    Removing Ajax Integration

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -83,65 +83,102 @@
 // });
 
 //Test
-$(function() {
+// $(function() {
 
+//   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+//     preventSubmit: true,
+//     submitError: function($form, event, errors) {
+//       // Additional error messages or events
+//     },
+//     submitSuccess: function($form, event) {
+//       event.preventDefault(); // Prevent default submit behaviour
+      
+//       // Get values from FORM
+//       var name = $("input#name").val();
+//       var email = $("input#email").val();
+//       var phone = $("input#phone").val();
+//       var website = $("input#website").val();  // Optional field
+//       var firstName = name; // For Success/Failure Message
+      
+//       // Check for white space in name for Success/Fail message
+//       if (firstName.indexOf(' ') >= 0) {
+//         firstName = name.split(' ').slice(0, -1).join(' ');
+//       }
+      
+//       $this = $("#sendMessageButton");
+//       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      
+//       // Since the form action is handled by Google Apps Script, we don't need custom Ajax handling here
+//       // The form will automatically submit to the Google Apps Script Web App URL
+      
+//       // Success message after Google Apps Script submission
+//       $('#success').html("<div class='alert alert-success'>");
+//       $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+//         .append("</button>");
+//       $('#success > .alert-success')
+//         .append("<strong>Your message has been sent. </strong>");
+//       $('#success > .alert-success')
+//         .append('</div>');
+      
+//       // Clear all fields
+//       $('#contactForm').trigger("reset");
+
+//       // Re-enable the submit button after 1 second
+//       setTimeout(function() {
+//         $this.prop("disabled", false);
+//       }, 1000);
+//     },
+//     filter: function() {
+//       return $(this).is(":visible");
+//     },
+//   });
+
+//   $("a[data-toggle=\"tab\"]").click(function(e) {
+//     e.preventDefault();
+//     $(this).tab("show");
+//   });
+// });
+
+// /* When clicking on Full hide fail/success boxes */
+// $('#name').focus(function() {
+//   $('#success').html('');
+// });
+
+//Test2
+$(function() {
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
-      // Additional error messages or events
+      // Handle error messages here if needed
     },
     submitSuccess: function($form, event) {
       event.preventDefault(); // Prevent default submit behaviour
-      
+
       // Get values from FORM
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
       var website = $("input#website").val();  // Optional field
-      var firstName = name; // For Success/Failure Message
       
-      // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-      }
-      
-      $this = $("#sendMessageButton");
-      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-      
-      // Since the form action is handled by Google Apps Script, we don't need custom Ajax handling here
-      // The form will automatically submit to the Google Apps Script Web App URL
-      
-      // Success message after Google Apps Script submission
+      // Prepare a success message
       $('#success').html("<div class='alert alert-success'>");
-      $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-        .append("</button>");
-      $('#success > .alert-success')
-        .append("<strong>Your message has been sent. </strong>");
-      $('#success > .alert-success')
-        .append('</div>');
-      
-      // Clear all fields
-      $('#contactForm').trigger("reset");
+      $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>");
+      $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
+      $('#success > .alert-success').append('</div>');
 
-      // Re-enable the submit button after 1 second
+      // Clear all fields after a delay
       setTimeout(function() {
-        $this.prop("disabled", false);
+        $('#contactForm').trigger("reset");
       }, 1000);
     },
     filter: function() {
       return $(this).is(":visible");
-    },
+    }
   });
 
-  $("a[data-toggle=\"tab\"]").click(function(e) {
-    e.preventDefault();
-    $(this).tab("show");
+  // Clear success message when user focuses on the name field
+  $('#name').focus(function() {
+    $('#success').html('');
   });
 });
-
-/* When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-  $('#success').html('');
-});
-
 
